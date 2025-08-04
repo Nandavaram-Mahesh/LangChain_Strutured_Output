bd8a9edb2f06748d364e21b9bffc7e3998307ec0
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 13:15:18 +0530

    Pydantic Field Functionality added

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -8,10 +8,11 @@ class Student(BaseModel):
     name:str='Nandavaram Mahesh'
     age:Optional[int]=None
     email:EmailStr
-       
+    cgpa:float = Field(gt=0, lt=10, default=5, description='A decimal value representing the cgpa of the student')
     
 new_student = {'age':29,'email':'aDkCt@gmail.com'}
 
 student = Student(**new_student)    
 
 print(student)
+# name='Nandavaram Mahesh' age=29 email='aDkCt@gmail.com' cgpa=5
