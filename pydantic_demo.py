ab8a7027f12b1ef86efb1e9c1c552477f9b12285
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 13:04:35 +0530

    Pydantic Default Values

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -4,13 +4,10 @@ from pydantic import BaseModel
 # It ensures that the data you are working with is correct,structured and type-safe.
 
 class Student(BaseModel):
-    name:str
+    name:str='Nandavaram Mahesh'
     
-new_student = {'name':"Nandavaram Mahesh"}
+new_student = {}
 
 student = Student(**new_student)    
 
-print(student)
-# name='Nandavaram Mahesh'
-print(type(student))
-# <class '__main__.Student'>  ----> Pydantic Object
+print(student.name)
