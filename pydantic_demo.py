87a8450e894b8106cf03761aaa42eac41f9969d3
Nandavaram-Mahesh <mahesh.nandavaram96@gmail.com>
2025-08-04 13:07:04 +0530

    Pydantic Optional Values

--- pydantic_demo.py
+++ pydantic_demo.py
@@ -1,13 +1,15 @@
 from pydantic import BaseModel
+from typing import Optional
 
 # Pydantic is a Data Validation and Serialization/parsing Framework for Python. 
 # It ensures that the data you are working with is correct,structured and type-safe.
 
 class Student(BaseModel):
     name:str='Nandavaram Mahesh'
+    age:Optional[int]=None    
     
-new_student = {}
+new_student = {'age':29}
 
 student = Student(**new_student)    
 
-print(student.name)
+print(student)
